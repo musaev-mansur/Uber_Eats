a1a31b3b20d1d0cd1a8b70ca798fe09a72c530bd
Maruev Muhammadhabib <123553664+Maruev-H@users.noreply.github.com>
2023-03-09 15:46:50 +0300

    полность всю верстку авторизации доделал

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Route } from 'react-router';
 import { Routes } from 'react-router-dom';
 import './App.scss';
 import './generalStyle/GeneralStyle.scss';
-import Authorization from './pages/Authorization/Authorization';
+import Registration from './pages/Authorization/Registration';
 import HomePage from './pages/HomePage/HomePage';
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <Routes >
         <Route path='/' element={<HomePage />}/>
-        <Route path='/auth' element={<Authorization />}/>
+        <Route path='/auth' element={<Registration />}/>
       </Routes>
     </div>
   );
