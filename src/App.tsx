c7364dbaca0ceda5eb12a4fe4b2e796dfa3e7ec7
Mansur-09595 <mansur_musaev_1997@mail.ru>
2023-03-10 16:59:40 +0300

    Завершено Delivery

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './App.scss';
 import './generalStyle/GeneralStyle.scss';
 import Registration from './pages/Authorization/Registration';
 import HomePage from './pages/HomePage/HomePage';
+import Delivery from './components/Delivery/Delivery';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes >
         <Route path='/' element={<HomePage />}/>
         <Route path='/sign-up' element={<Registration />}/>
+        <Route path='/delivery' element={<Delivery />}/>
       </Routes>
     </div>
   );
