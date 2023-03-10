266e5b8c00fc0197870f6ff064c755d09614192c
Mansur-09595 <mansur_musaev_1997@mail.ru>
2023-03-10 22:47:55 +0300

    Добавил Личный кабинет переходы

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { useAppSelector } from "../../hooks/hooks";
 import logos from "../../images/logos.png"
+import profile from "../../images/profile.png"
 import calling from "../../images/calling.png"
 import "./Navbar.scss"
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    
+  const { currentUser } = useAppSelector(state => state.user)
+
   return (
     <>
       <div className="header">
@@ -21,12 +25,28 @@ const Navbar = () => {
             <div className="contact">
                 Контакты:
                 <p>+7 (917) 510-57-59</p>
-            </div> 
+            </div>
+            <div className="profile">
+                <img src={profile} alt="" />
+                <a href="">{currentUser.role ? (`${currentUser.role}`) : (<Link className="turn-off" to="/sign-in">Войти</Link>)}</a>
+            </div>
             <button className="basket greenBack">
                 <span className="basket-text">Корзина</span>
                 <hr />
                 <p><span className="number">4</span></p>
             </button>
+
+            {currentUser.id && (
+              <button className="personal greenBack">
+                <span>
+                  {currentUser.id ? (
+                    <Link className="turn-off" to={`/personal/${currentUser.id}`}>Личный кабинет</Link>
+                  ) : (
+                    <span>Личный кабинет</span>
+                  )}
+                </span>
+              </button>
+            )}
         </div>
       </div>
       <hr className="navbar-hr"/>
