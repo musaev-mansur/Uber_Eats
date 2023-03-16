a6104f4054349949ee6498fe0c82ce964b47bc1b
Maruev Muhammadhabib <123553664+Maruev-H@users.noreply.github.com>
2023-03-16 14:21:40 +0300

    убрал все лишнее

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -2,15 +2,14 @@ import React, { useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import { useAppSelector } from "../../hooks/hooks";
 import { userApi } from "../../store/reducers/servise/userServise";
-import avatar from "../../images/avatar.png"
-
+import avatar from "../../images/avatar.png";
 import "./Profile.scss";
 import Menu from "./Menu/Menu";
 import Orders from "./Orders/Orders";
 import Information from "./Information/Information";
 
 const Profile = () => {
-  const [ content, setContent ] = useState("orders")
+  const [content, setContent] = useState("orders");
   const { role } = useAppSelector((state) => state.user.currentUser);
   const { error, data } = userApi.useGetUserQuery(role);
 
@@ -18,24 +17,37 @@ const Profile = () => {
     <div>
       <Navbar />
       <div className="profile">
-        
         {content === "orders" && <Orders />}
         {content === "menu" && <Menu />}
         {content === "information" && <Information />}
 
         <div className="right-profile">
-          <div className="userImage">
-            <img src={avatar} alt="avatar" />
+          <div className="userImage userImage">
+            {data?.image && <img src={data?.image} alt="avatar" />}
           </div>
           <p>{data?.name}</p>
           <div className="booking-links">
-            <p onClick={() => setContent("orders")} className={`button ${content === "orders" && `greenBack`}`}>Заказы</p>
-            <p onClick={() => setContent("menu")} className={`button ${content === "menu" && `greenBack`}`}>Меню</p>
-            <p onClick={() => setContent("information")} className={`button ${content === "information" && `greenBack`}`}>Информация о ресторане</p>
+            <p
+              onClick={() => setContent("orders")}
+              className={`button ${content === "orders" && `greenBack`}`}
+            >
+              Заказы
+            </p>
+            <p
+              onClick={() => setContent("menu")}
+              className={`button ${content === "menu" && `greenBack`}`}
+            >
+              Меню
+            </p>
+            <p
+              onClick={() => setContent("information")}
+              className={`button ${content === "information" && `greenBack`}`}
+            >
+              Информация о ресторане
+            </p>
           </div>
         </div>
       </div>
-        
     </div>
   );
 };
--- src/store/reducers/food/foodSlice.ts
+++ src/store/reducers/food/foodSlice.ts
@@ -1,21 +1,9 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { cookies } from "../../../Api/api";
-import { foodState, IFood } from "../../../types/IFood";
-import {updateFood } from "./foodActions";
+import { updateFood } from "./foodActions";
 
-const initialState: foodState = {
-  food: [{
-    _id: cookies.get('_id') || '',
-    name: "",
-    image: "",
-    mail: "", 
-    phone: "",
-    city: "",
-    address: "",
-    menu: [],
-  }],
-  isAuth: false,
-  isLoading: false,
+const initialState = {
+ 
 };
 
 export const foodSlice = createSlice({
@@ -23,24 +11,7 @@ export const foodSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-     //updateTodo
-     builder.addCase(updateFood.pending, (state) => {
-      state.isLoading = true;
-    });
-    builder.addCase(updateFood.fulfilled, (state, action: PayloadAction<IFood>) => {
-      state.isLoading = false
-      cookies.set('_id', action.payload._id)
-      state.food = state.food.map((post: IFood) => {
-        if (post._id === action.payload._id) {
-          return action.payload;
-        }
-        return post;
-      });
-    });
-    builder.addCase(updateFood.rejected, (state, action) => {
-      state.isLoading = false;
-      // state.error = action.error.message || "Something went wrong";
-    });
+    
 
 }});
 
