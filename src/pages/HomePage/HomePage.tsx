9706de641a6310810cae7b329f258a538abd53d0
Maruev Muhammadhabib <123553664+Maruev-H@users.noreply.github.com>
2023-03-23 15:34:43 +0300

    сделал правки по стилям

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -4,14 +4,11 @@ import Navbar from "../../components/Navbar/Navbar";
 import TopCaffe from "../../components/TopCaffe/TopCaffe";
 import Products from "../../components/Products/Products";
 import Footer from "../../components/Footer/Footer";
-import EmptyBasket from "../../modalWindows/EmptyBasket/EmptyBasket";
-
 const HomePage = () => {
     
   return (
     <>
       <Navbar />
-      <EmptyBasket />
       <TopCaffe />
       <Products />
       <Footer />
